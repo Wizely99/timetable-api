e8f40559a9ff415149c281f2567eb865aef5b1bd
jackkweyunga <jackkweyunga@gmail.com>
2023-07-09 17:40:15 +0300

    updated settings

--- config/settings.py
+++ config/settings.py
@@ -274,4 +274,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", deault="")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")

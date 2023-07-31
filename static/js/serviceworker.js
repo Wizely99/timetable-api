51487b90e8e22430ca259090a9d1b5c77b219bdb
Wizely99 <100375920+Wizely99@users.noreply.github.com>
2023-07-31 23:26:49 +0300

    fix:service workers
    made service worker fetch data while online and serve it for view when offline

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -1,4 +1,5 @@
-var staticCacheName = 'djangopwa-v2';
+var staticCacheName = 'djangopwa-v3';
+var dynamicCacheName = 'dynamicCacheName-v2';
 
 self.addEventListener('install', function (event) {
     console.log("ServiceWorker installed");
@@ -12,16 +13,34 @@ self.addEventListener('install', function (event) {
 });
 
 self.addEventListener('fetch', function (event) {
-    var requestUrl = new URL(event.request.url);
-    if (requestUrl.origin === location.origin) {
-        if ((requestUrl.pathname === '/landing')) {
-            event.respondWith(caches.match('/'));
-            return;
-        }
-    }
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        })
-    );
+    //var requestUrl = new URL(event.request.url);
+    // if (requestUrl.origin === location.origin) {
+    //     if ((requestUrl.pathname === '/landing')) {
+    //         event.respondWith(caches.match('/'));
+    //         return;
+    //     }
+    // }
+
+
+    self.addEventListener('fetch', function (event) {
+        event.respondWith(
+            // Try fetching the request from the server first.
+            fetch(event.request)
+            .then(function (fetchRes) {
+                // If fetch is successful, clone the response and cache it before returning it to the page.
+                const clonedResponse = fetchRes.clone();
+                caches.open(dynamicCacheName)
+                    .then(function (cache) {
+                        cache.put(event.request, clonedResponse);
+                    });
+
+                return fetchRes;
+            })
+            .catch(function () {
+                // If fetch fails (e.g., when the user is offline), try to retrieve the response from the cache.
+                return caches.match(event.request);
+            })
+        );
+    });
+
 });

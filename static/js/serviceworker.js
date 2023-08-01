96f2a48194668e198776a8d54b5a6a7e96f2b636
Wizely99 <100375920+Wizely99@users.noreply.github.com>
2023-08-01 14:33:00 +0300

    added templates from memplas design

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -1,46 +1,55 @@
-var staticCacheName = 'djangopwa-v3';
-var dynamicCacheName = 'dynamicCacheName-v2';
+// service-worker.js
 
-self.addEventListener('install', function (event) {
-    console.log("ServiceWorker installed");
-    event.waitUntil(
-        caches.open(staticCacheName).then(function (cache) {
-            return cache.addAll([
-                '/',
-            ]);
-        })
-    );
-});
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/7.0.0/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js');
 
-self.addEventListener('fetch', function (event) {
-    //var requestUrl = new URL(event.request.url);
-    // if (requestUrl.origin === location.origin) {
-    //     if ((requestUrl.pathname === '/landing')) {
-    //         event.respondWith(caches.match('/'));
-    //         return;
-    //     }
-    // }
+const staticCacheName = 'timetableStaticCacheName-v4';
+const dynamicCacheName = 'timetableDynamicCacheName-v5';
+const documentCacheName = 'timetableHtmlCacheName-v5';
+
+workbox.routing.registerRoute(
+    ({
+        request
+    }) => request.destination === 'document',
+    new workbox.strategies.NetworkFirst({
+        cacheName: documentCacheName,
+    })
+);
+
+workbox.routing.registerRoute(
+    ({
+        request
+    }) => request.destination === 'script' || request.destination === 'style',
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: staticCacheName,
+    })
+);
+
+workbox.routing.registerRoute(
+    ({
+        request
+    }) => request.destination === 'image',
+    new workbox.strategies.CacheFirst({
+        cacheName: staticCacheName,
+    })
+);
+
+workbox.routing.registerRoute(
+    ({
+        request
+    }) => request.destination === 'font',
+    new workbox.strategies.CacheFirst({
+        cacheName: staticCacheName,
+    })
+);
 
 
-    self.addEventListener('fetch', function (event) {
-        event.respondWith(
-            // Try fetching the request from the server first.
-            fetch(event.request)
-            .then(function (fetchRes) {
-                // If fetch is successful, clone the response and cache it before returning it to the page.
-                const clonedResponse = fetchRes.clone();
-                caches.open(dynamicCacheName)
-                    .then(function (cache) {
-                        cache.put(event.request, clonedResponse);
-                    });
 
-                return fetchRes;
-            })
-            .catch(function () {
-                // If fetch fails (e.g., when the user is offline), try to retrieve the response from the cache.
-                return caches.match(event.request);
-            })
-        );
-    });
-
-});
+workbox.routing.registerRoute(
+    ({
+        request
+    }) => true, // Catch-all for other requests
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: dynamicCacheName,
+    })
+);
